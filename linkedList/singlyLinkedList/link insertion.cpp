08d014745339253b436a6c22cbd5ae502ec68fe3
Fariya Ahmed punom <fariyaahmed874@gmail.com>
2024-06-27 23:34:46 +0600

    Add files via upload

---
+++ linkedList/singlyLinkedList/link insertion.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node *next;
+    node(int value)
+    {
+        data=value;
+        next=NULL;
+    }
+};
+void print(node *&head)
+{
+    node *temp=head;
+    int count=0;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+        count++;
+    }
+    cout<<endl;
+    cout<<"Number of linked list:"<<count;
+    cout<<endl;
+}
+void insert_head(node*&head,int d)
+{
+ node*temp=new node(d);
+ temp->next=head;
+ head=temp;
+
+}
+void insert_tail(node*&tail,int d)
+{
+ node*temp=new node(d);
+ tail->next=temp;
+ tail=temp;
+
+}
+int main()
+{
+    node*node1=new node(20);
+    node*head=node1;
+    insert_head(head,30);
+    insert_head(head,40);
+    //insert_head(head,50);
+    insert_head(head,60);
+    print(head);
+    cout<<endl;
+    node*tail=node1;
+    //insert_tail(tail,20);
+    //insert_tail(tail,30);
+    insert_tail(tail,40);
+    insert_tail(tail,50);
+    print(head);
+}

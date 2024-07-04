717f39ad93fdf87456eae5228fa9cccd45aabf37
Fariya Ahmed punom <fariyaahmed874@gmail.com>
2024-07-04 12:18:42 +0600

    Add files via upload

---
+++ linkedList/singlyLinkedList/Fiding length .cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node*next;
+    node(int value)
+    {
+        data=value;
+        next=NULL;
+    }
+};
+insert_head(node*&head,int data)
+{
+    node*temp=new node(data);
+    temp->next=head;
+    head=temp;
+}
+int get_length(node*&head)
+{
+    node*temp=head;
+    int len=0;
+    while(temp!=NULL)
+    {
+        len++;
+        temp=temp->next;
+    }
+    return len;
+}
+int main()
+{
+    node*node1=new node(10);
+    node*head=node1;
+    insert_head(head,20);
+    insert_head(head,30);
+    insert_head(head,40);
+    insert_head(head,50);
+    cout<<"Length of linked list is: "<<get_length(head);
+}

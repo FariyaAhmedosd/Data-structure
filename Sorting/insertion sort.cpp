ef4110cd0decd9d080dd6c6451f967b1bee6fa3e
Fariya Ahmed punom <fariyaahmed874@gmail.com>
2024-07-26 22:06:20 +0600

    Add files via upload

---
+++ Sorting/insertion sort.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+ int arr[10],n;
+ void input()
+ {
+     cout<<"Enter size: ";
+     cin>>n;
+     cout<<"Enter array elements: ";
+     for(int i=0;i<n;i++)
+     {
+         cin>>arr[i];
+     }
+ }
+ void insertion_sort()
+ {
+     for(int i=0;i<n;i++)
+     {
+         int key=arr[i];
+         int j=i-1;
+         while(key>arr[j]&&j>=0)
+         {
+             arr[j+1]=arr[j];
+             j--;
+         }
+         arr[j+1]=key;
+     }
+
+ }
+ void print()
+    {
+    for(int j=0;j<n;j++)
+    {
+        cout<<arr[j]<<" ";
+    }
+    }
+int main()
+{
+    input();
+    cout<<"Before sorting elements are: ";
+    print();
+    cout<<endl;
+    insertion_sort();
+    cout<<"After sorting elements are: ";
+    print();
+
+}
